a4da4e2797a931d84b6c48817ce1cb1d7d44b0aa
thavao <othavioact@gmail.com>
2024-04-18 23:14:18 -0300

    refatorando mensagens;

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,28 +10,30 @@ while (true)
 
     if (opcao != 0)
     {
-        Console.WriteLine("Digite o 1º numero");
+        Console.Write("Digite o 1º numero: ");
         numero1 = double.Parse(Console.ReadLine());
 
-        Console.WriteLine("Digite o 2º numero");
+        Console.Write("Digite o 2º numero: ");
         numero2 = double.Parse(Console.ReadLine());
 
         if (opcao == 1)
-            Console.WriteLine($"{numero1} + {numero2} = {numero1 + numero2}");
+            Console.WriteLine($"\n{numero1} + {numero2} = {numero1 + numero2}\n");
 
         if (opcao == 2)
-            Console.WriteLine($"{numero1} - {numero2} = {numero1 - numero2}");
+            Console.WriteLine($"\n{numero1} - {numero2} = {numero1 - numero2}\n");
 
         if (opcao == 3)
-            Console.WriteLine($"{numero1} * {numero2} = {numero1 * numero2}");
+            Console.WriteLine($"\n{numero1} * {numero2} = {numero1 * numero2}\n");
 
         if (opcao == 4)
             if (numero2 != 0)
-                Console.WriteLine($"{numero1} / {numero2} = {numero1 / numero2}");
+                Console.WriteLine($"\n{numero1} / {numero2} = {numero1 / numero2}\n");
             else
-                Console.WriteLine("Não é possível dividir por 0");
+                Console.WriteLine("\nNão é possível dividir por 0\n");
 
     }
+    else if(opcao > 4 || opcao < 0)
+        Console.WriteLine("Opção inválida, digite novamente...");
     else
         break;
 }
