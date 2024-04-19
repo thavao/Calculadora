71345fd5f85213aeff8a54d37e0d3e29a386e0cd
thavao <othavioact@gmail.com>
2024-04-18 22:54:57 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,16 @@
+﻿double a, b;
+
+Console.WriteLine("Digite o 1º numero");
+a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o 2º numero");
+b = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"{a} + {b} = {a + b}");
+Console.WriteLine($"{a} - {b} = {a - b}");
+Console.WriteLine($"{a} * {b} = {a * b}");
+Console.WriteLine($"{a} / {b} = {a / b}");
+
+Console.WriteLine("Pressione Enter para encerrar...");
+
+Console.ReadLine();
